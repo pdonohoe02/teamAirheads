3e20d65cefcb4a57725df5c970c0bc4446fddf30
espeer <espeer@caltech.edu>
2023-03-04 02:31:43 -0800

    add pods, begin implemented DB

--- airheadsFrontend/airheadsFrontend/airheadsFrontendApp.swift
+++ airheadsFrontend/airheadsFrontend/airheadsFrontendApp.swift
@@ -6,14 +6,35 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuthUI
+import FirebaseEmailAuthUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    let authUI = FUIAuth.defaultAuthUI()
+    authUI.delegate = self
+    let providers: [FUIAuthProvider] = [
+    FUIEmailAuth(),
+    ]
+    //self.authUI.providers = providers
+    return true
+  }
+}
 
 @main
-struct airheadsFrontendApp: App {
-    //@StateObject private var modelData = ModelData()
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                //.environmentObject(modelData)
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
