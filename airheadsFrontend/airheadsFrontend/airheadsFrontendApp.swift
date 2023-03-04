fd94d4d2e35519a1c940fd75bd75a9bd746bb1ef
pdonohoe02 <pdonohoe@caltech.edu>
2023-03-03 22:17:47 -0800

    initial commit

---
+++ airheadsFrontend/airheadsFrontend/airheadsFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  airheadsFrontendApp.swift
+//  airheadsFrontend
+//
+//  Created by Patrick Donohoe on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct airheadsFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
