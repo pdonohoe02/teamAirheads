1ca32a7f42fa2007ab92228baa48e583df1a25ff
pdonohoe02 <pdonohoe@caltech.edu>
2023-03-03 22:37:53 -0800

    Added files for each tab

---
+++ airheadsFrontend/airheadsFrontend/Views/RequestRide/RequestRideHome.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileHome.swift
+//  Rideshare
+//
+//  Created by Patrick Donohoe on 3/3/23.
+//
+
+import SwiftUI
+
+struct RequestRideHome: View {
+    
+    var body: some View {
+        
+        NavigationView{
+            ScrollView{
+                //.foregroundColor(.gray)
+                VStack{
+                    
+                    Image("logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 130, height: 130)
+                        .cornerRadius(100)
+                        .padding(22)
+                        .padding(.top, -80)
+                }
+                VStack{
+                    Text("Name Here")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                }
+                VStack{
+                    Text("Preferences")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .offset(x: 30, y: 0)
+                }
+                .frame(maxWidth:.infinity, maxHeight: .infinity,
+                       alignment: .leading)
+            }
+        }
+    }
+}
